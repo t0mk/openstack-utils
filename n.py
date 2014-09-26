64e71dc95263c57ccd337ea0f80a186e88698062
Tomas Karasek <tomas.karasek@digile.fi>
2014-09-26 13:50:57 +0300

    handle when instance doesn't have a public ip

--- n.py
+++ n.py
@@ -14,7 +14,10 @@ def main(args_list):
     with open(cache_file, 'r') as f:
         for l in f:
             fields = l.strip().split(' ', 2)
-            print fields[0], json.loads(fields[2])['floating'][0], fields[1]
+            float_ip = json.loads(fields[2])['floating']
+            if not float_ip:
+                float_ip = '[NO_PUBLIC_IPS]'
+            print fields[0], float_ip , fields[1]
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv[1:]))
